3b281b01bb7e5905097ba35fb131ac0494e64a06
Thiago-Almeida23 <thiagocldealmeida@gmail.com>
2024-04-16 18:35:14 -0300

    Estilizando a SideBar.

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/Thiago-Almeida23.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Thiago-Almeida23.png" />
-    <Titulo fontSize={20}>Thiago Almeida</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Thiago Almeida</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Thiago-Almeida23
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack - Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
